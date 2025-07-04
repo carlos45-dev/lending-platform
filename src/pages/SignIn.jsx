3793105b0104c8f78f995d773c4f06a2e5e5fdc8
carlos muleke <carlosmuleke@gmail.com>
2025-07-04 22:10:20 +0200

    Lending-page

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -2,6 +2,8 @@ import '../styles/SignIn.css';
 import img from '../assets/favicon.ico';
 import eye from '../assets/eye.png';
 import eye2 from '../assets/eye2.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser,faLock } from '@fortawesome/free-solid-svg-icons';
 
 import { useState } from 'react';
 function SignIn(){
@@ -25,11 +27,13 @@ function SignIn(){
         <div className="form-group">
             <label htmlFor="email">Student Email:</label>
             <input type="email" id="email" name="email" required className='input' placeholder='Enter your email' />
+            <FontAwesomeIcon className='user' icon={faUser} />
         </div>
 
         <div className="form-group2">
             <label htmlFor="password">Password:</label>
             <input type={showpassword ? "text" : "password"} id="password" name="password" className='input' required  placeholder='Enter your password'/>
+            <FontAwesomeIcon className='lock' icon={faLock} />
             <span className='eye-container' onClick={handlepasswordToggle}>
            <img className='eye' src={showpassword ? eye : eye2} alt="eye icon" />
             </span>
