1bc9065540c486068a72591723ecdf307b9e9f8f
carlos muleke <carlosmuleke@gmail.com>
2025-07-04 14:08:44 +0200

    Lending-platform

---
+++ src/pages/SignIn.jsx
@@ -0,0 +1,20 @@
+import '../styles/SignIn.css';
+function SignIn(){
+ return(
+    <div className="signin-container">
+        <h3>Sign In</h3>
+        <form>
+        <div className="form-group">
+            <label htmlFor="email">Student Email:</label><br />
+            <input type="email" id="email" name="email" required className='input' />
+        </div>
+        <div className="form-group2">
+            <label htmlFor="password">Password:</label><br />
+            <input type="password" id="password" name="password" className='input' required />
+        </div>
+        <button className='submit' type="submit">Sign In</button>
+        </form>
+    </div>
+ )
+}
+export default SignIn
