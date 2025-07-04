9c984f217359377cfec734a77abd888562327e45
carlos muleke <carlosmuleke@gmail.com>
2025-07-04 15:53:09 +0200

    Lending-platform

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -5,12 +5,13 @@ function SignIn(){
         <h3>Sign In</h3>
         <form>
         <div className="form-group">
-            <label htmlFor="email">Student Email:</label><br />
+            <label htmlFor="email">Student Email:</label>
             <input type="email" id="email" name="email" required className='input' placeholder='Enter your email' />
         </div>
         <div className="form-group2">
-            <label htmlFor="password">Password:</label><br />
+            <label htmlFor="password">Password:</label>
             <input type="password" id="password" name="password" className='input' required  placeholder='Enter your password'/>
+            <a className='forgot' >Forgot password</a>
         </div>
         <button className='submit' type="submit">Sign In</button>
         </form>
