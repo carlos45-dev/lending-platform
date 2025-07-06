ffaa42520fa411aa85cf2244642ae3333adab2fe
carlos muleke <carlosmuleke@gmail.com>
2025-07-06 19:23:15 +0200

    sign up

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,56 +1,68 @@
-import { useEffect } from 'react';
+import { useState } from 'react';
+import ReCAPTCHA from 'react-google-recaptcha';
 import styles from '../styles/SignUp.module.css';
 
-
 function Signup() {
+  const [recaptchaValue, setRecaptchaValue] = useState(null);
+  const key = "6LeZCXorAAAAAG7KhM3jjBoyWRANY4SE5AMh6rXF";
 
- const key = "6LeZCXorAAAAAG7KhM3jjBoyWRANY4SE5AMh6rXF";
+  const handleRecaptchaChange = (value) => {
+    setRecaptchaValue(value);
+    console.log("reCAPTCHA value:", value);
+  };
 
   return (
-    <div className={styles.signup}>
-      <div className={styles.header}>
-        <h1>Sign Up</h1>
+    <>
+      <h2>Get started with your account</h2>
+      <div className={styles.signup}>
+        <div className={styles.header}>
+          <h1>Sign Up</h1>
+        </div>
+        <form>
+          <div className={styles.formGroup}>
+            <input type="text" className={styles.input} placeholder="Username" />
+          </div>
+
+          <div className={styles.formGroup}>
+            <input type="email" className={styles.input} placeholder="Email" />
+          </div>
+
+          <div className={styles.formGroup}>
+            <input type="password" className={styles.input} placeholder="Password" />
+          </div>
+
+          <div className={styles.formGroup}>
+            <input type="password" className={styles.input} placeholder="Confirm Password" />
+          </div>
+
+          <div className={styles.gender}>
+            <input type="radio" name="gender" value="male" id="male" />
+            <label htmlFor="male" className={styles.radio}>Male</label>
+
+            <input type="radio" name="gender" value="female" id="female" />
+            <label htmlFor="female" className={styles.radio}>Female</label>
+
+            <input type="radio" name="gender" value="other" id="other" />
+            <label htmlFor="other" className={styles.radio}>Other</label>
+          </div>
+
+          <div className={styles.checkbox}>
+            <input type="checkbox" id="terms" />
+            <label htmlFor="terms">I agree to the terms and conditions</label>
+          </div>
+
+        
+          <div className={styles.recaptchaWrapper}>
+            <ReCAPTCHA
+              sitekey={key}
+              onChange={handleRecaptchaChange}
+            />
+          </div>
+
+          <button type="submit" className={styles.submit}>Sign Up</button>
+        </form>
       </div>
-      <form>
-        <div className={styles.formGroup}>
-          <input type="text" className={styles.input} placeholder="Username" />
-        </div>
-
-        <div className={styles.formGroup}>
-          <input type="email" className={styles.input} placeholder="Email" />
-        </div>
-
-        <div className={styles.formGroup}>
-          <input type="password" className={styles.input} placeholder="Password" />
-        </div>
-        <div className={styles.formGroup}>
-          <input type="password" className={styles.input} placeholder="Confirm Password" />
-        </div>
-
-        <div className={styles.gender}>
-                <input type="radio" name="gender" value="male" id ="male" />
-                <label for="male" className={styles.radio}>Male</label>
-
-                <input type="radio" name="gender" value="other" id ="female" />
-                <label for="female" className={styles.radio}>Female</label>
-
-                <input type="radio" name="gender" value="other" id ="other" />
-                <label for="other" className={styles.radio}>Other</label>
-        </div>
-
-        <div className={styles.checkbox}>
-          <input type="checkbox" id="terms" />
-          <label for="terms">I agree to the terms and conditions</label>
-        </div>
-
-        <div
-            className="g-recaptcha"
-            data-sitekey={key}
-        ></div>
-
-        <button type="submit" className={styles.submit}>Sign Up</button>
-      </form>
-    </div>
+    </>
   );
 }
 
