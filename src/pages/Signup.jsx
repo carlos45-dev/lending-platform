32b11f745ce023a9294a852b190cfce566782c4a
carlos muleke <carlosmuleke@gmail.com>
2025-07-06 23:00:50 +0200

    sign in

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,68 +1,90 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import ReCAPTCHA from 'react-google-recaptcha';
 import styles from '../styles/SignUp.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faLock, faEyeSlash, faEye } from '@fortawesome/free-solid-svg-icons';
 
 function Signup() {
   const [recaptchaValue, setRecaptchaValue] = useState(null);
+  const [showPassword, setShowPassword] = useState(false);
+
   const key = "6LeZCXorAAAAAG7KhM3jjBoyWRANY4SE5AMh6rXF";
 
   const handleRecaptchaChange = (value) => {
     setRecaptchaValue(value);
-    console.log("reCAPTCHA value:", value);
+  };
+
+  const handlePasswordToggle = () => {
+    setShowPassword(!showPassword);
   };
 
   return (
     <>
-      <h2>Get started with your account</h2>
+      <h2 style={{ textAlign: 'center', marginBottom: '20px' }}>Get started with your account</h2>
       <div className={styles.signup}>
-        <div className={styles.header}>
-        </div>
         <form>
           <div className={styles.formGroup}>
             <input type="text" className={styles.input} placeholder="Username" required />
+            <FontAwesomeIcon className={styles["input-icon"]} icon={faUser} />
           </div>
 
           <div className={styles.formGroup}>
-            <input type="email" className={styles.input} placeholder="Email" required/>
+            <input type="email" className={styles.input} placeholder="Email" required />
+            <FontAwesomeIcon className={styles["input-icon"]} icon={faUser} />
           </div>
 
           <div className={styles.formGroup}>
-            <input type="password" className={styles.input} placeholder="Password" required />
+            <input
+              type={showPassword ? "text" : "password"}
+              className={styles.input}
+              placeholder="Password"
+              required
+            />
+            <FontAwesomeIcon className={styles["input-icon2"]} icon={faLock} />
+            <span className={styles["eye-container"]} onClick={handlePasswordToggle}>
+              {showPassword ? <FontAwesomeIcon icon={faEyeSlash} /> : <FontAwesomeIcon icon={faEye} />}
+            </span>
           </div>
 
           <div className={styles.formGroup}>
-            <input type="password" className={styles.input} placeholder="Confirm Password" required />
+            <input
+              type={showPassword ? "text" : "password"}
+              className={styles.input}
+              placeholder="Confirm Password"
+              required
+            />
+            <FontAwesomeIcon className={styles["input-icon2"]} icon={faLock} />
+            <span className={styles["eye-container"]} onClick={handlePasswordToggle}>
+              {showPassword ? <FontAwesomeIcon icon={faEyeSlash} /> : <FontAwesomeIcon icon={faEye} />}
+            </span>
           </div>
 
           <div className={styles.gender}>
+            <div className={styles.gender2}>
+              <input type="radio" name="gender" value="male" id="male" />
+              <label htmlFor="male" className={styles.label}>Male</label>
+            </div>
 
             <div className={styles.gender2}>
-            <input type="radio"  name="gender" value="male" id="male" />
-            <label htmlFor="male" className={styles.label}>Male</label>
-            </div>
-             
-             <div className={styles.gender2}>
-            <input type="radio" name="gender" value="female" id="female" />
-            <label htmlFor="female" className={styles.label}>Female</label>
-            </div>
-            
-            <div className={styles.gender2}>
-            <input type="radio" name="gender" value="other" id="other" />
-            <label htmlFor="other" className={styles.label}>Other</label>
+              <input type="radio" name="gender" value="female" id="female" />
+              <label htmlFor="female" className={styles.label}>Female</label>
             </div>
 
+            <div className={styles.gender2}>
+              <input type="radio" name="gender" value="other" id="other" />
+              <label htmlFor="other" className={styles.label}>Other</label>
+            </div>
           </div>
 
           <div className={styles.checkbox}>
             <input type="checkbox" id="terms" required />
-            <label  className={styles.label2} htmlFor="terms" style={{ marginLeft: '5px' }}>I agree to the terms and conditions of UniFund</label>
+            <label htmlFor="terms" className={styles.label2}>
+              I agree to the terms and conditions of UniFund
+            </label>
           </div>
 
           <div className={styles.recaptchaWrapper}>
-            <ReCAPTCHA
-              sitekey={key}
-              onChange={handleRecaptchaChange}
-            />
+            <ReCAPTCHA sitekey={key} onChange={handleRecaptchaChange} />
           </div>
 
           <button type="submit" className={styles.submit}>Sign Up</button>
