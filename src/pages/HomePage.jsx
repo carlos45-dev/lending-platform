04ff427763f1af834a25b45f557d53f4a24a4aba
carlos muleke <carlosmuleke@gmail.com>
2025-07-09 21:47:14 +0200

    home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -19,8 +19,8 @@ function HomePage(){
       <>
         <Header/>
         <div className={styles.homeImage}>
-        <img src={img} alt="home Image" style={{borderRadius: '20px', height:'200px',
-        width:'350px'}} />
+        <img src={img} alt="home Image" style={{borderRadius: '7px', height:'35vh',
+        width:'96vw'}} />
         </div>
         <div className={styles.servicesContainer}>
             
