599fa9baaa5afc5b7bbb02ddb34fd3ba237aac4c
carlos muleke <carlosmuleke@gmail.com>
2025-07-11 22:06:09 +0200

    profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,22 @@
-import { useState, useEffect} from "react";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import styles from "../styles/Profile.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEnvelope, faSignOutAlt, faShareAlt, faInfoCircle, faCamera, faPhone,faLock } from "@fortawesome/free-solid-svg-icons";
-import user  from '../assets/user.jfif';
+import {
+  faEnvelope,
+  faSignOutAlt,
+  faShareAlt,
+  faInfoCircle,
+  faCamera,
+  faPhone,
+  faLock,
+  faArrowLeft,
+} from "@fortawesome/free-solid-svg-icons";
+import user from "../assets/user.jfif";
 
 function Profile() {
   const [image, setImage] = useState(user);
+  const navigate = useNavigate();
 
   const handleImageUpload = (e) => {
     const file = e.target.files[0];
@@ -14,19 +25,25 @@ function Profile() {
     }
   };
 
-   //resetting the display settings
-   useEffect(() => {
-                 const originalDisplay = document.body.style.display;
-                 document.body.style.display = 'block';
-                 const originalColor = document.body.style.backgroundColor;
-                 document.body.style.backgroundColor = '#1A2258';
-                 return () => {
-                 document.body.style.display = originalDisplay;
-                 document.body.style.backgroundColor = originalColor;
-                   };
-      }, []);
-     return (
+  useEffect(() => {
+    const originalDisplay = document.body.style.display;
+    document.body.style.display = "block";
+    const originalColor = document.body.style.backgroundColor;
+    document.body.style.backgroundColor = "#1A2258";
+    return () => {
+      document.body.style.display = originalDisplay;
+      document.body.style.backgroundColor = originalColor;
+    };
+  }, []);
+
+  return (
     <div className={styles.profileContainer}>
+      <FontAwesomeIcon
+        icon={faArrowLeft}
+        className={styles.backArrow}
+        onClick={() => navigate("/")}
+      />
+
       <h1 className={styles.title}>Profile</h1>
 
       <div className={styles.imageWrapper}>
@@ -47,39 +64,37 @@ function Profile() {
         />
       </div>
 
+      <div className={styles.information}>
+        <p className={styles.name}>Carlos Muleke</p>
 
-     <div className={styles.information}>
-      <p className={styles.name}>Carlos Muleke</p>
-      <div className={styles.detail}>
-        <FontAwesomeIcon icon={faEnvelope} />
-        <p>example2@gmail.com</p>
-      </div>
-
-      <div className={styles.detail}>
-        <FontAwesomeIcon icon={faPhone} />
-        <p>+265992779377</p>
-      </div>
-
-      <div className={styles.actions}>
-
-        <div>
-          <FontAwesomeIcon icon={faLock} />
-          <p>Reset Password</p>
+        <div className={styles.detailLeft}>
+          <FontAwesomeIcon icon={faEnvelope} />
+          <span>Email:</span>
+          <p>example2@gmail.com</p>
         </div>
 
-        <div>
-          <FontAwesomeIcon icon={faSignOutAlt} />
-          <p>Log Out</p>
+        <div className={styles.detailLeft}>
+          <FontAwesomeIcon icon={faPhone} />
+          <span>Phone:</span>
+          <p>+265992779387</p>
         </div>
 
-        <div>
-          <FontAwesomeIcon icon={faShareAlt} />
-          <p>Share</p>
-        </div>
-
-        <div>
-          <FontAwesomeIcon icon={faInfoCircle} />
-          <p>About Us</p>
+        <div className={styles.actions}>
+          <div>
+            <FontAwesomeIcon icon={faLock} />
+            <p>Reset Password</p>
+          </div>
+          <div>
+            <FontAwesomeIcon icon={faSignOutAlt} />
+            <p>Log Out</p>
+          </div>
+          <div>
+            <FontAwesomeIcon icon={faShareAlt} />
+            <p>Share</p>
+          </div>
+          <div>
+            <FontAwesomeIcon icon={faInfoCircle} />
+            <p>About Us</p>
           </div>
         </div>
       </div>
